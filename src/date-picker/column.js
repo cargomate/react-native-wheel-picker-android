bdb9c60b30b31f1d3990fd8377bf405972ca9148
Dennis Sandmark <d.sandmark@gmail.com>
2019-04-30 14:32:47 +0100

    Updating hard-coded props

--- src/date-picker/column.js
+++ src/date-picker/column.js
@@ -1,15 +1,17 @@
 import WheelPicker from "../wheel-picker";
 import React from "react";
 
-export const titles = array => array.map( item => item.title );
+export const titles = array => array.map(item => item.title);
 
-export default ( { list, ...props } ) => (
-    <WheelPicker
-        data={ titles( list ) }
-        isAtmospheric
-        isCurved
-        selectedItemTextColor={ "black" }
-        visibleItemCount={7}
-        { ...props }
-    />
+export default ({ list, ...props }) => (
+  <WheelPicker
+    data={titles(list)}
+    isAtmospheric
+    isCurved
+    {...props}
+    itemTextSize={100}
+    itemTextColor={"white"}
+    selectedItemTextColor={"white"}
+    visibleItemCount={5}
+  />
 );
