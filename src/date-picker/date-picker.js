8ef9872302f120611e52ca7b94a6bd7235154540
Dennis Sandmark <d.sandmark@gmail.com>
2019-09-06 16:15:15 +0100

    Fixing issue where the wrong date was selected

--- src/date-picker/date-picker.js
+++ src/date-picker/date-picker.js
@@ -8,120 +8,110 @@ import styles from "./styles";
 import DateColumns from "./date-columns";
 import { normalizeDate, normalizeMinMaxDates } from "./utils";
 
-const dateSetters = {
-    "year": "setFullYear",
-    "month": "setMonth",
-    "date": "setDate",
-    "hours": "setHours",
-    "minutes": "setMinutes"
-};
-
 export default class DatePicker extends PureComponent {
+  static propTypes = {
+    mode: PropTypes.oneOf(["date", "time", "datetime"]).isRequired,
+    date: PropTypes.instanceOf(Date),
+    // TODO: initialDate
+    onDateChange: PropTypes.func.isRequired,
+    minimumDate: PropTypes.instanceOf(Date),
+    maximumDate: PropTypes.instanceOf(Date),
+    minuteInterval: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30]),
+    styles: PropTypes.object,
+    locale: PropTypes.string,
+    todayTitle: PropTypes.string
+  };
 
-    static propTypes = {
-        mode: PropTypes.oneOf( [ "date", "time", "datetime" ] ).isRequired,
-        date: PropTypes.instanceOf( Date ),
-        // TODO: initialDate
-        onDateChange: PropTypes.func.isRequired,
-        minimumDate: PropTypes.instanceOf( Date ),
-        maximumDate: PropTypes.instanceOf( Date ),
-        minuteInterval: PropTypes.oneOf( [ 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30 ] ),
-        styles: PropTypes.object,
-        locale: PropTypes.string,
-        todayTitle: PropTypes.string
+  static defaultProps = {
+    mode: "date",
+    todayTitle: "Today",
+    minuteInterval: 1
+  };
+
+  constructor(props, ...args) {
+    super(props, ...args);
+
+    let date = (props.date && new Date(props.date.valueOf())) || new Date();
+    const { minimumDate, maximumDate } = normalizeMinMaxDates(
+      date,
+      props.minimumDate,
+      props.maximumDate
+    );
+    date = normalizeDate(date, minimumDate, maximumDate);
+
+    this.state = {
+      date,
+      minimumDate,
+      maximumDate,
+      timeOnly: props.mode === "time",
+      dateOnly: props.mode === "date"
     };
 
-    static defaultProps = {
-        mode: "date",
-        todayTitle: "Today",
-        minuteInterval: 1
-    };
+    this.styles = styles(props.styles);
+  }
 
+  render() {
+    return (
+      <View style={[this.styles.container, this.props.style]}>
+        {this.renderDate()}
+        {this.renderTime()}
+      </View>
+    );
+  }
 
-    constructor( props, ...args ) {
-        super( props, ...args );
+  renderDate() {
+    const { date, minimumDate, maximumDate, dateOnly, timeOnly } = this.state;
+    if (timeOnly) return null;
 
-        let date = props.date && new Date( props.date.valueOf() ) || new Date();
-        const { minimumDate, maximumDate } = normalizeMinMaxDates( date, props.minimumDate, props.maximumDate );
-        date = normalizeDate( date, minimumDate, maximumDate );
+    const { locale, todayTitle } = this.props;
+    return (
+      <DateColumns
+        dateOnly={dateOnly}
+        date={date}
+        minimumDate={minimumDate}
+        maximumDate={maximumDate}
+        onChange={this.onChange}
+        styles={this.styles}
+        locale={locale}
+        todayTitle={todayTitle}
+      />
+    );
+  }
 
-        this.state = {
-            date,
-            minimumDate,
-            maximumDate,
-            timeOnly: props.mode === "time",
-            dateOnly: props.mode === "date"
-        };
+  renderTime() {
+    const { date, dateOnly, timeOnly } = this.state;
+    if (dateOnly) return null;
 
-        this.styles = styles( props.styles );
+    const { locale, minuteInterval } = this.props;
+    return (
+      <TimeColumns
+        timeOnly={timeOnly}
+        date={date}
+        minuteInterval={minuteInterval}
+        onChange={this.onChange}
+        styles={this.styles}
+        locale={locale}
+      />
+    );
+  }
+
+  onChange = change => {
+    const { minimumDate, maximumDate } = this.state;
+    let date = this.state.date;
+
+    if (change.year && change.month && change.date) {
+      date.setFullYear(change.year, change.month, change.date);
     }
 
-    render() {
-        return (
-            <View style={ [ this.styles.container, this.props.style ] }>
-                { this.renderDate() }
-                { this.renderTime() }
-            </View>
-        )
+    if (change.hours && change.minutes) {
+      date.setHours(change.hours);
+      date.setMinutes(change.minutes);
     }
 
-    renderDate() {
-        const { date, minimumDate, maximumDate, dateOnly, timeOnly } = this.state;
-        if ( timeOnly )
-            return null;
+    date = normalizeDate(date, minimumDate, maximumDate);
 
-        const { locale, todayTitle } = this.props;
-        return (
-            <DateColumns
-                dateOnly={ dateOnly }
-                date={ date }
-                minimumDate={ minimumDate }
-                maximumDate={ maximumDate }
-                onChange={ this.onChange }
-                styles={ this.styles }
-                locale={ locale }
-                todayTitle={ todayTitle }
-            />
-        );
-    }
+    if (date !== this.state.date) this.setState({ date });
 
-    renderTime() {
-        const { date, dateOnly, timeOnly } = this.state;
-        if ( dateOnly )
-            return null;
-
-        const { locale, minuteInterval } = this.props;
-        return (
-            <TimeColumns
-                timeOnly={ timeOnly }
-                date={ date }
-                minuteInterval={ minuteInterval }
-                onChange={ this.onChange }
-                styles={ this.styles }
-                locale={ locale }
-            />
-        );
-    }
-
-    onChange = change => {
-        const { minimumDate, maximumDate } = this.state;
-        let date = this.state.date;
-
-        Object.keys( dateSetters )
-            .forEach( key => {
-                const value = change[ key ];
-                if ( value !== undefined ) {
-                    date[ dateSetters[ key ] ]( value );
-                }
-            } )
-        ;
-
-        date = normalizeDate( date, minimumDate, maximumDate );
-
-        if ( date !== this.state.date )
-            this.setState( { date } );
-
-        this.props.onDateChange( new Date( date.valueOf() ) );
-    };
-
+    this.props.onDateChange(new Date(date.valueOf()));
+  };
 }
